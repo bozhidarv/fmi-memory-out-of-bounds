7ada3052e7d9dc07797b2b99a9c11016bdc82857
Bozhidar Videnov <bozh.bv03@gmail.com>
2023-03-18 12:58:00 +0200

    added immovable object and changed some configuration

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -11,15 +11,22 @@ export default class MainScene extends Phaser.Scene
     {
         this.load.image('background', 'assets/basic_background.png')
         this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png')
+        this.load.multiatlas('chest', 'assets/chests-assets.json', 'assets')
     }
 
     create()
     {
-        const background = this.add.image(0, 0, 'background')
-        background.displayHeight = this.game.canvas.height*2;
-        background.displayWidth = this.game.canvas.width*2;
+        const background = this.add.image(0, 0, 'background');
+        background.setScale(5.5, 5)
+        // background.displayHeight = this.game.canvas.height;
+        // background.displayWidth = this.game.canvas.width;
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+        const chest = this.physics.add.sprite(500, 500, 'chest', 'chests.jpg');
+        chest.setScale(0.5, 0.5);
+        chest.body.immovable = true;
+        const logo = this.physics.add.image(400, 100, 'logo');
+        this.physics.add.collider(chest, logo);
+
 
         logo.setVelocity(100, 200)
         logo.setBounce(1, 1)
