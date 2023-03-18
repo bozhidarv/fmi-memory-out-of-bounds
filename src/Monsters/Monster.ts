abb4a510bee94a2162fc2d4a04518efd89c27915
Mihail Tsankov <mihail.tsankov@trading212.com>
2023-03-18 12:20:11 +0200

    added basic monster

---
+++ src/Monsters/Monster.ts
@@ -0,0 +1,20 @@
+import { generateSingleDigitEq } from "~/services/EquationGenerator";
+import { Sprite } from "~/services/type";
+
+export class Monster {
+  sprite: Sprite = {} as Sprite;
+  equation: string = "";
+  lives: number[] = [];
+
+  constructor(x: number, y: number, game: Phaser.Scene) {
+    const gen = generateSingleDigitEq();
+    this.equation = gen.equation;
+    this.lives = gen.answer;
+
+    this.sprite = game.physics.add.sprite(x, y, "monster");
+  }
+
+  move(): void {
+    this.sprite.setVelocity(5);
+  }
+}
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,28 +1,41 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
+import { Monster } from "~/Monsters/Monster";
 
-export default class MainScene extends Phaser.Scene
-{
-	constructor()
-	{
-		super('hello-world')
-	}
+export default class MainScene extends Phaser.Scene {
+  monsters: Monster[] = [];
 
-	preload()
-    {
-        this.load.image('background', 'assets/basic_background.png')
-        this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png')
-    }
+  constructor() {
+    super("hello-world");
+  }
+  preload() {
+    this.load.image("background", "assets/basic_background.png");
+    this.load.image(
+      "logo",
+      "http://labs.phaser.io/assets/sprites/phaser3-logo.png"
+    );
+    this.load.image("monster", "assets/monster.jpeg");
+  }
 
-    create()
-    {
-        const background = this.add.image(0, 0, 'background')
-        background.displayHeight = this.game.canvas.height*2;
-        background.displayWidth = this.game.canvas.width*2;
+  create() {
+    const background = this.add.image(0, 0, "background");
+    background.displayHeight = window.innerHeight * 2;
+    background.displayWidth = window.innerHeight * 4;
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+    const logo = this.physics.add.image(400, 100, "logo");
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-    }
+    this.monsters.push(new Monster(100, 100, this));
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+  }
+
+  update(time: number, delta: number) {
+    this.monsters.forEach((monster) => {
+      monster.move();
+    });
+  }
 }
