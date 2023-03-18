ae6406bd402b0f3e9e2e91e73f6b3ed082eae3be
Mihail Tsankov <mihail.tsankov@trading212.com>
2023-03-18 16:33:08 +0200

    added Monster config

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -2,9 +2,39 @@ import Phaser from "phaser";
 import { SmallMonster } from "~/Monsters/SmallMonster";
 import { Player } from "~/players/player";
 import { Health } from "~/players/health";
-
+import { SceneMonstersConfigT } from "~/services/type";
 
 let counter = 0;
+
+const monsterConfig: SceneMonstersConfigT = {
+  smallMonsters: [
+    {
+      startX: 100,
+      startY: 100,
+    },
+    {
+      startX: 200,
+      startY: 100,
+    },
+    {
+      startX: 300,
+      startY: 100,
+    },
+    {
+      startX: 400,
+      startY: 500,
+    },
+    {
+      startX: 500,
+      startY: 600,
+    },
+    {
+      startX: 600,
+      startY: 700,
+    },
+  ],
+  bigMonsters: [],
+};
 export default class MainScene extends Phaser.Scene {
   monsters: SmallMonster[] = [];
   player: Player = {} as Player;
@@ -27,8 +57,6 @@ export default class MainScene extends Phaser.Scene {
 
     this.health = new Health(this);
 
-
-
     this.player = new Player(100, 100, this);
 
     this.generateMonsters();
@@ -37,15 +65,11 @@ export default class MainScene extends Phaser.Scene {
   }
 
   generateMonsters() {
-    for (let index = 0; index < 10; index++) {
-      this.monsters.push(
-        new SmallMonster(
-          Math.random() * window.innerHeight,
-          Math.random() * window.innerHeight,
-          this
-        )
-      );
-    }
+    this.monsters = this.monsters.concat(
+      monsterConfig.smallMonsters.map(
+        (monster) => new SmallMonster(monster.startX, monster.startY, this)
+      )
+    );
 
     const monsterSprites = this.monsters.map(
       (monster) => monster.body.mainSprite
@@ -56,12 +80,11 @@ export default class MainScene extends Phaser.Scene {
   update(time, delta) {
     this.player.move();
     // console.log(time);
-    if(counter === 0 || counter === 50) {
+    if (counter === 0 || counter === 50) {
       this.player.shoot();
       counter = 0;
     }
     counter++;
-
 
     this.monsters.forEach((monster) => {
       monster.move(this.player, this);
--- src/services/type.ts
+++ src/services/type.ts
@@ -1,3 +1,13 @@
 export type Sprite = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
-export type CursorT = Phaser.Types.Input.Keyboard.CursorKeys
+export type CursorT = Phaser.Types.Input.Keyboard.CursorKeys;
+
+type MonsterConfigT = {
+  startX: number;
+  startY: number;
+};
+
+export type SceneMonstersConfigT = {
+  smallMonsters: MonsterConfigT[];
+  bigMonsters: MonsterConfigT[];
+};
