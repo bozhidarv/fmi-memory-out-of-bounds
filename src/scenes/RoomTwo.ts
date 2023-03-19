d16aea9e9058ff7788e1d346f50cd008ebdc95b5
MartinM2304 <m.marinkov6@mail.bg>
2023-03-19 14:23:32 +0200

    room coordinates repaired

--- src/scenes/RoomTwo.ts
+++ src/scenes/RoomTwo.ts
@@ -96,7 +96,7 @@ export default class RoomTwo extends Phaser.Scene {
   }
 
   generatePlayer() {
-    this.player = new Player(100, 100, this, this.playerData);
+    this.player = new Player(100, window.innerHeight/2, this, this.playerData);
   }
 
   generateFss() {
